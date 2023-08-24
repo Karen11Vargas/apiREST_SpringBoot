307183a4a86c9ec30f198ac9b746d4964ab8d712
Karen Lizeth Vargas <101535088+Karen11Vargas@users.noreply.github.com>
2023-08-24 16:35:57 -0500

    Metodos guardar y buscar por un id

--- src/main/java/com/example/springBoot/controller/BookController.java
+++ src/main/java/com/example/springBoot/controller/BookController.java
@@ -2,10 +2,13 @@ package com.example.springBoot.controller;
 
 import com.example.springBoot.entities.Book;
 import com.example.springBoot.repository.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -26,5 +29,27 @@ public class BookController {
     }
 
     //Devolver solo uno
-    //public Book findOneById(Long id)
+    @GetMapping("/api/books/{id}")
+    public ResponseEntity<Book> findOneById(@PathVariable  Long id){
+        Optional<Book> book = repository.findById(id);
+
+        //Opcion 1
+        //Se devuelve un ok con la data tipo json
+        if (book.isPresent())
+            return ResponseEntity.ok(book.get());
+        else
+            //Construye una respuesta de no found
+            return ResponseEntity.notFound().build();
+
+        //opcion 2, reducida
+        //return book.orElse(null);
+    }
+
+
+    //Guardar libro
+    @PostMapping("/api/books")
+    public Book crear(@RequestBody Book book, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        return repository.save(book);
+    }
 }
