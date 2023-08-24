26df975575a38e14156dddbef6597fac1d610f9c
Karen Lizeth Vargas <101535088+Karen11Vargas@users.noreply.github.com>
2023-08-24 00:12:41 -0500

    Hacer la funcion de findAll en el controlador

--- src/main/java/com/example/springBoot/controller/BookController.java
+++ src/main/java/com/example/springBoot/controller/BookController.java
@@ -1,7 +1,32 @@
 package com.example.springBoot.controller;
 
+import com.example.springBoot.entities.Book;
+import com.example.springBoot.repository.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
+
+    //atributos
+    private BookRepository repository;
+
+    //Constructor
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    //Buscar los libros
+    @GetMapping("/api/books")
+    public List<Book> findAll(){
+        //recuperar y devolver los lobros de bd
+        return repository.findAll();
+    }
+
+    //Devolver solo uno
+    public Book findOneById(Long id){
+
+    }
 }
