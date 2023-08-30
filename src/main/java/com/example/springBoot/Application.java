19d1111cc9c58c33b161164059237c4bf6efc930
Karen Lizeth Vargas <101535088+Karen11Vargas@users.noreply.github.com>
2023-08-30 12:50:18 -0500

    Documentascion de la api con javadocs

--- src/main/java/com/example/springBoot/Application.java
+++ src/main/java/com/example/springBoot/Application.java
@@ -1,7 +1,13 @@
 package com.example.springBoot;
 
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +16,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(){
+		return new OpenAPI()
+				.info(new Info()
+						.title("Spring Boot Api REST")
+						.version("1.0")
+						.description("Library Api rest docs")
+						.termsOfService("http://www.google.com")
+				);
+
+
+	}
 }
